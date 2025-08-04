960ed6450a8de1a43be0f6dcb82196a94dcd4250
Pedrogrm <pedro.m.alves@aluno.senai.br>
2025-08-04 14:25:18 -0300

    Delete Python directory

--- Python/HelloWorld.py
+++
@@ -1,8 +0,0 @@
-# É uma tradição no mundo da programação começar com um programa simples chamado "Olá Mundo". Este programa simplesmente mostra a mensagem "Olá Mundo" na tela. 
-
-print("Hello World ☺︎ ")
-
-# o python é capaz de realizar operações matemáticas simples. Aqui está um exemplo:
-print(1 + 1)  # Exemplo de uma operação matemática simples, que soma 1 + 1 e imprime o resultado.
-
-
